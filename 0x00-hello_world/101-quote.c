6d7b5e6b7b420291e8706c84210492bd896357ca
ahmed-sbihi <sbihiahmed20@gmail.com>
2023-09-11 15:36:50 -0700

    task8

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <unistd.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * Return: Always 1 (error code)
+ */
+int main(void)
+{
+    char *str = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+    write(2, str, 59);
+    return (1);
+}
