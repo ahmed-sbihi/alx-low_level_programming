e7cce4ec8dc375576f4839d733139fb0255f697a
ahmed-sbihi <sbihiahmed20@gmail.com>
2023-09-13 15:58:42 -0700

    task2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -5,17 +5,17 @@
  */
 void print_alphabet_x10(void)
 {
-    char letter = 'a';
-    int i, j;
+	char letter = 'a';
+	int i, j;
 
-    for (i = 0; i < 10; i++)
-    {
-        for (j = 0; j < 26; j++)
-        {
-            _putchar(letter);
-            letter++;
-        }
-        letter = 'a'; /* Reset letter to 'a' for the next line */
-        _putchar('\n');
-    }
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 0; j < 26; j++)
+		{
+			_putchar(letter);
+			letter++;
+		}
+		letter = 'a'; /* Reset letter to 'a' for the next line */
+		_putchar('\n');
+	}
 }
