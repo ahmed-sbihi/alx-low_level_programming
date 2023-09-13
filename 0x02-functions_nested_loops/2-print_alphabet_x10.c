c776c465eaff8d5aad13accd4e4a17fdfb17649a
ahmed-sbihi <sbihiahmed20@gmail.com>
2023-09-13 15:48:16 -0700

    task2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - prints the alphabet in lowercase 10 times
+ */
+void print_alphabet_x10(void)
+{
+    char letter = 'a';
+    int i, j;
+
+    for (i = 0; i < 10; i++)
+    {
+        for (j = 0; j < 26; j++)
+        {
+            _putchar(letter);
+            letter++;
+        }
+        letter = 'a'; // Reset letter to 'a' for the next line
+        _putchar('\n');
+    }
+}
