38d1f431a577611deb9de8419b0f331476588e4f
ahmed-sbihi <sbihiahmed20@gmail.com>
2023-09-13 17:55:32 -0700

    task9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -22,13 +22,13 @@ void times_table(void)
             if (result < 10)
             {
                 _putchar(' ');
-                _putchar('0' + result);
             }
             else
             {
                 _putchar('0' + result / 10);
-                _putchar('0' + result % 10);
             }
+
+            _putchar('0' + result % 10);
         }
         _putchar('\n');
     }
