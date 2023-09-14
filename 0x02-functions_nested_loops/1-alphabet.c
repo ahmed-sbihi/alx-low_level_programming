cb9979d621fe0f83b9f8dd32333c5525bb5fca90
ahmed-sbihi <sbihiahmed20@gmail.com>
2023-09-14 04:12:44 -0700

    task1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,16 +1,16 @@
 #include "main.h"
-
 /**
- * print_alphabet - this is the function of the task
- * Return: return 0
+ * print_alphabet - prints the alphabet in lowercase 
+ *
+ * Return: Always 0 (success)
  */
 void print_alphabet(void)
 {
-	char c;
+	int  i;
 
-	for (c = 'a'; c <= 'z'; c++)
+	for (i = 97; i <= 122; i++)
 	{
-		_putchar(c);
+		_putchar(i);
 	}
 
 	_putchar('\n');
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,8 +1,5 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-/* Function prototypes */
-void print_times_table(int n);
 int _putchar(char c);
-
-#endif /* MAIN_H */
+void print_alphabet(void);
