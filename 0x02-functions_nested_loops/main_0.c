4ec5e93aefc7c85253dd1a68c8639631d2470906
ahmed-sbihi <sbihiahmed20@gmail.com>
2023-09-13 18:43:09 -0700

    task11

--- 0x02-functions_nested_loops/main_0.c
+++ 0x02-functions_nested_loops/main_0.c
@@ -1,7 +1,8 @@
-#include "holberton.h"
+#include <stdio.h>
+#include "main.h"
 
 /**
- * main - check the code
+ * main - check the code for Holberton School students.
  *
  * Return: Always 0.
  */
