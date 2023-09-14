55293c32c786816ea1c8692163fb0d75749bf6d1
ahmed-sbihi <sbihiahmed20@gmail.com>
2023-09-13 17:58:00 -0700

    task9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -5,31 +5,31 @@
  */
 void times_table(void)
 {
-    int i, j, result;
+	int i, j, result;
 
-    for (i = 0; i <= 9; i++)
-    {
-        for (j = 0; j <= 9; j++)
-        {
-            result = i * j;
+	for (i = 0; i <= 9; i++)
+	{
+		for (j = 0; j <= 9; j++)
+		{
+			result = i * j;
 
-            if (j != 0)
-            {
-                _putchar(',');
-                _putchar(' ');
-            }
+			if (j != 0)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
 
-            if (result < 10)
-            {
-                _putchar(' ');
-            }
-            else
-            {
-                _putchar('0' + result / 10);
-            }
+			if (result < 10)
+			{
+				_putchar(' ');
+			}
+			else
+			{
+				_putchar('0' + result / 10);
+			}
 
-            _putchar('0' + result % 10);
-        }
-        _putchar('\n');
-    }
+			_putchar('0' + result % 10);
+		}
+		_putchar('\n');
+	}
 }
