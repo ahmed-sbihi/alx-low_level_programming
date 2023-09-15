a7f6d197f1e3a1aad2760b5efe3f9584c4b2384a
ahmed-sbihi <sbihiahmed20@gmail.com>
2023-09-15 09:25:52 -0700

    task5

--- 0x02-functions_nested_loops/5-main.c
+++ 0x02-functions_nested_loops/5-main.c
@@ -19,7 +19,7 @@ int main(void)
     _putchar(' ');
     _putchar(r + '0');
     _putchar('\n');
-    r = print_sign(255);  /* Changed to 255, which is a positive number */
+    r = print_sign(0xff);
     _putchar(',');
     _putchar(' ');
     _putchar(r + '0');
