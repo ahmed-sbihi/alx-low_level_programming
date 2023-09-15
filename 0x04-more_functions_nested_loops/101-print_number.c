00db406cc8b1f7f4aa19ecc7f4859c9f9ca8d315
ahmed-sbihi <sbihiahmed20@gmail.com>
2023-09-14 19:05:53 -0700

    task12

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -6,16 +6,22 @@
  */
 void print_number(int n)
 {
-	if (n < 0)
-	{
-		_putchar('-');
-		n = -n;
-	}
+    int sign = 1;
+    unsigned int num;
 
-	if (n / 10 != 0)
-	{
-		print_number(n / 10);
-	}
+    if (n < 0)
+    {
+        _putchar('-');
+        sign = -1;
+        num = -n;
+    }
+    else
+    {
+        num = n;
+    }
 
-	_putchar((n % 10) + '0');
+    if (num / 10)
+        print_number(num / 10);
+
+    _putchar(num % 10 + '0');
 }
